139b88317429ce9942c9795e5a78ef8eb1c0728d
NIRAJSHARMA0509 <120631030+NIRAJSHARMA0509@users.noreply.github.com>
2022-12-24 20:48:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,33 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
+  const [startTime, setStartTime] = useState(0);
+
+  useEffect(()=>{
+    const timer = startTime > 0  && setTimeout(()=>setStartTime((prevState) => {return prevState - 1}), 1000);
+    return () => clearTimeout(timer);
+  },[startTime]);
+    const setTimer = (event) => {
+      if(event.keyCode === 13){
+        if(isNaN(event.target.value))
+          setStartTime(0);
+        else{
+          let num = Math.floor(event.target.value);
+          setStartTime(num);
+        }
+      }
+    };
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for
+          <input id="timeCount"
+          onKeyDown = {(event) => {setTimer(event)}}/>sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{startTime}}</div>
     </div>
   )
 }
